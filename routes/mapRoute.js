7e23f381a8fe41e8da8d3f1a679ce75897a704e6
Sophia Kapchinsky <s.kapchinsky@gmail.com>
2024-02-06 13:42:23 -0500

    Cleaned up code

--- routes/mapRoute.js
+++ routes/mapRoute.js
@@ -1,38 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-
+const axios = require("axios");
 const knex = require("knex");
 const knexConfig = require("../knexfile.js");
-const knexInstance = knex(knexConfig);
 
-// console.log("key", process.env.MAP_KEY)
 
 // MAP API
-router.get("/", async (req, res)=>{
+router.get("/", async (req, res) => {
   try {
-    const response = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json`, {
-      params: {
-        keyword: 'doctor',
-        location: req.query.location,
-        radius: 1500,
-        type: 'doctor',
-        key: process.env.MAP_KEY
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json`,
+      {
+        params: {
+          keyword: "doctor",
+          location: req.query.location,
+          radius: 1500,
+          type: "doctor",
+          key: process.env.MAP_KEY,
+        },
       }
-    });
+    );
 
     let limitedResults = response.data.results.slice(0, 6);
-
-    let detailedResults = await Promise.all(limitedResults.map(async result => {
-      let detailResponse = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json`, {
-        params: {
-          place_id: result.place_id,
-          key: process.env.MAP_KEY
-        }
-      });
-      return { ...result, detail: detailResponse.data.result };
-    }));
-    console.log("results for FE: ", detailedResults)
+    let detailedResults = await Promise.all(
+      limitedResults.map(async (result) => {
+        let detailResponse = await axios.get(
+          `https://maps.googleapis.com/maps/api/place/details/json`,
+          {
+            params: {
+              place_id: result.place_id,
+              key: process.env.MAP_KEY,
+            },
+          }
+        );
+        return { ...result, detail: detailResponse.data.result };
+      })
+    );
     res.json({ results: detailedResults });
   } catch (error) {
     res.status(500).send(error.toString());
--- server.js
+++ server.js
@@ -6,13 +6,12 @@ const questionsRoute = require("./routes/questionsRoute.js");
 const mapRoute = require("./routes/mapRoute.js");
 dotenv.config();
 const { PORT, CORS_ORIGIN } = process.env || 5050;
-console.log("cors:",CORS_ORIGIN )
+console.log("cors:", CORS_ORIGIN);
 
 app.use(cors({ origin: CORS_ORIGIN }));
 app.use(express.json());
-
-app.use("/question", questionsRoute)
-app.use("/map", mapRoute)
+app.use("/question", questionsRoute);
+app.use("/map", mapRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
